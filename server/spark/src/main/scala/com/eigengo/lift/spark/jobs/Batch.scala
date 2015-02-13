01572a790c1a26a2cfc5e36ffc31dbd2830c4071
martinzapletal <zapletal-martin@email.cz>
2015-02-13 15:27:30 +0000

    Startup

--- server/spark/src/main/scala/com/eigengo/lift/spark/jobs/Batch.scala
+++ server/spark/src/main/scala/com/eigengo/lift/spark/jobs/Batch.scala
@@ -1,6 +1,13 @@
 package com.eigengo.lift.spark.jobs
 
-import com.typesafe.config.Config
+import com.typesafe.config.{ConfigFactory, Config}
+import org.apache.spark.SparkContext
+
+object Batch {
+  def apply[P, R](func: (SparkContext, P) => Either[String, R])(config: Config = ConfigFactory.empty()) = {
+    ???
+  }
+}
 
 trait Batch[P, R] extends App {
 
