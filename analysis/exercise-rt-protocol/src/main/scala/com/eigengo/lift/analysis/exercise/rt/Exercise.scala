d5a3347cca256ebd9922908d8eecdb7f826673b2
Jan Machacek <jan.machacek@gmail.com>
2015-02-20 22:03:01 +0000

    Restructure and tidy-up

--- analysis/exercise-rt-protocol/src/main/scala/com/eigengo/lift/analysis/exercise/rt/Exercise.scala
+++
@@ -1,8 +0,0 @@
-package com.eigengo.lift.analysis.exercise.rt
-
-/**
- * The exercise with the given name and intensity
- * @param name the exercise name
- * @param intensity the exercise intensity
- */
-case class Exercise(name: String, intensity: Double)
