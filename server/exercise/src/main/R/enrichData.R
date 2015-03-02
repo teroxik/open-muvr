dd78c79ec3a30c16941ce3babae75ace27223c37
Mark Petruska <markp@cakesolutions.net>
2015-03-02 17:17:35 +0000

    implements enrichData function

--- server/exercise/src/main/R/enrichData.R
+++ server/exercise/src/main/R/enrichData.R
@@ -12,4 +12,16 @@ readDataCsv = function(input) {
   read.csv(file = input, header = TRUE)
 }
 
-
+# Enriches the data frame with function ``f``, called with
+# ``windowSize`` rows.
+#
+# @param data       the input data
+# @param windowSize the size of the sliding window
+# @param colName    the name of the column where the data will be stored
+# @param f          the function that calculates the enriched values
+enrichData = function(data, windowSize, colName, f) {
+  for (i in windowSize:nrow(data)) {
+    data[i, colName] = f(data[(i - windowSize + 1):i,])
+  }
+  data
+}
