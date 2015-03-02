645b5bbff62f445493c25b290fccbaa4e8194abe
Mark Petruska <markp@cakesolutions.net>
2015-03-02 15:34:47 +0000

    adds description to readDataCsv

--- server/exercise/src/main/R/enrichData.R
+++ server/exercise/src/main/R/enrichData.R
@@ -5,6 +5,11 @@
 #
 ########################################################################################################################
 
+# Reads data frame from the provided csv file.
+#
+# @param input  path of the input file
 readDataCsv = function(input) {
   read.csv(file = input, header = TRUE)
 }
+
+
