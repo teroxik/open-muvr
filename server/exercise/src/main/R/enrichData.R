26a0ef8dd553b3de77523e1ba311b68b48c14f09
Mark Petruska <markp@cakesolutions.net>
2015-03-03 10:55:17 +0000

    implements signal vector magnitude
    implements pipe forward operator
    implements enrichDataWithAllFeatures

--- server/exercise/src/main/R/enrichData.R
+++ server/exercise/src/main/R/enrichData.R
@@ -5,31 +5,84 @@
 #
 ########################################################################################################################
 
+log = TRUE
+
+# Writes a log message if logging is enabled.
+#
+# @param message    the message
+writeLogMessage = function(message) {
+  if (log) {
+   write(message, stdout())
+  }
+}
+
+# String concatenation operator
+"%+%" = paste0
+
+# Forward composition operator
+# x %|>% f = f(x)
+#
+# @param x  the value to apply to ``f``
+# @param f  the function to evaluate with ``x``
+"%|>%" = function (x, f) {
+  f(x)
+}
+
 # Reads data frame from the provided csv file.
 #
 # @param input  path of the input file
 readDataCsv = function(input) {
+  writeLogMessage("Reading data from input file...")
   read.csv(file = input, header = TRUE)
 }
 
 # Enriches the data frame with function ``f``, called with
 # ``windowSize`` rows.
 #
-# @param data       the input data
 # @param windowSize the size of the sliding window
 # @param colName    the name of the column where the data will be stored
 # @param f          the function that calculates the enriched values
-enrichData = function(data, windowSize, colName, f) {
-  for (i in windowSize:nrow(data)) {
-    data[i, colName] = f(data[(i - windowSize + 1):i,])
+# @param data       the input data
+enrichData = function(windowSize, colName, f) {
+  force(f)
+  function(data) {
+    writeLogMessage("Calculating " %+% colName %+% " ...")
+    for (i in windowSize:nrow(data)) {
+      data[i, colName] = f(data[(i - windowSize + 1):i,])
+    }
+    data
   }
-  data
 }
 
 # Saves data frame to csv file ``output``.
 #
 # @param output     the path of the output file
 # @param data       the data frame to output
-saveDataToCsv = function(output, data) {
-  write.table(data, file = output, row.names = FALSE, sep = ",")
+saveDataToCsv = function(output) {
+  function(data) {
+    writeLogMessage("Saving data to output file...")
+    write.table(data, file = output, row.names = FALSE, sep = ",")
+  }
+}
+
+# Calculates the signal vector magnitude of the
+# samples. Calculation is done on a sample basis
+# (ignores sliding window).
+#
+# @param data       the input data subset
+signalVectorMagnitude = function(data) {
+  sqrt( (data[1, "x"] ^ 2) + (data[1, "y"] ^ 2) + (data[1, "z"] ^ 2) )
+}
+
+# Enriches data with all implemented features.
+#
+# @param inputFile      path of the input csv file
+# @param outputFile     path of the output csv file
+enrichDataWithAllFeatures = function(inputFile, outputFile) {
+  inputFile %|>%
+  readDataCsv %|>%
+  enrichData(1, "SVM", signalVectorMagnitude) %|>%
+  # TODO: add more feature calculations here
+  saveDataToCsv(outputFile)
+  TRUE
 }
